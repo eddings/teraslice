4893ac5ba2baab2bf15db9bd7f18fcffb933fd74
peterdemartini <thepeterdemartini@gmail.com>
2019-09-10 09:42:39 -0700

    Refactor code in entities

--- packages/utils/src/entities/utils.ts
+++ packages/utils/src/entities/utils.ts
@@ -1,4 +1,5 @@
 import * as i from './interfaces';
+import { isPlainObject } from '../objects';
 import { isString } from '../strings';
 import { isNumber } from '../numbers';
 
@@ -62,5 +63,17 @@ export function isValidKey(key: any): key is string|number {
 }
 
 export function isDataEntity(input: any): boolean {
-    return Boolean(input != null && input[i.__IS_DATAENTITY_KEY] === true);
+    return Boolean(input != null && input[i.__IS_ENTITY_KEY] === true);
+}
+
+export function isDataWindow(input: any): boolean {
+    return Boolean(input != null && input[i.__IS_WINDOW_KEY] === true);
+}
+
+export function canConvertToEntityArray(input: any): boolean {
+    if (input == null) return false;
+    if (Array.isArray(input)) return true;
+    if (isDataEntity(input)) return true;
+    if (isPlainObject(input)) return true;
+    return false;
 }
